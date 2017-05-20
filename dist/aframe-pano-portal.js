5506262720b265ff34341e9da1fdc592a6251abb
PyryM <pkmatikainen@gmail.com>
2017-05-20 14:59:06 -0400

    more intuitive warp params

--- dist/aframe-pano-portal.js
+++ dist/aframe-pano-portal.js
@@ -67,7 +67,7 @@
 /* 1 */
 /***/ (function(module, exports) {
 
-	module.exports = "uniform vec4 warpParams;\r\nvarying vec3 worldNormal;\r\nvarying vec3 worldViewDir;\r\nvarying float warpAlpha;\r\n\r\nvoid main(void) {\r\n  vec4 zeroPos = modelMatrix * vec4(0.0, 0.0, 0.0, 1.0);\r\n  float distToZero = length(cameraPosition - zeroPos.xyz);\r\n  warpAlpha = clamp(warpParams.x * distToZero + warpParams.y, 0.0, 1.0);\r\n  vec4 worldPos = modelMatrix * vec4(position, 1.0);\r\n  worldViewDir = worldPos.xyz - cameraPosition;\r\n  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\r\n  worldNormal = (modelMatrix * vec4(normal.xyz, 0.0)).xyz;\r\n}\r\n"
+	module.exports = "uniform vec4 warpParams;\r\nvarying vec3 worldNormal;\r\nvarying vec3 worldViewDir;\r\nvarying float warpAlpha;\r\n\r\nvoid main(void) {\r\n  vec4 zeroPos = modelMatrix * vec4(0.0, 0.0, 0.0, 1.0);\r\n  float distToZero = length(cameraPosition - zeroPos.xyz);\r\n  float dd = (distToZero - warpParams.x) / (warpParams.y - warpParams.x);\r\n  warpAlpha = clamp(dd, 0.0, 1.0);\r\n  vec4 worldPos = modelMatrix * vec4(position, 1.0);\r\n  worldViewDir = worldPos.xyz - cameraPosition;\r\n  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\r\n  worldNormal = (modelMatrix * vec4(normal.xyz, 0.0)).xyz;\r\n}\r\n"
 
 /***/ }),
 /* 2 */
@@ -79,7 +79,7 @@
 /* 3 */
 /***/ (function(module, exports) {
 
-	module.exports = "#define M_PI 3.1415926535897932384626433832795\r\nuniform sampler2D src;\r\nuniform vec4 warpParams;\r\nvarying vec3 worldNormal;\r\nvarying vec3 worldViewDir;\r\nvarying float warpAlpha;\r\n\r\nfloat rand(vec2 co){\r\n  return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);\r\n}\r\n\r\nvec2 panoMap(vec3 vdir) {\r\n  float r = length(vdir.xz);\r\n  float theta = atan(vdir.z, vdir.x) / M_PI;\r\n  float phi = atan(vdir.y, r) / M_PI;\r\n  vec2 uv = vec2(theta*0.5 + 0.5, phi + 0.5);\r\n  return uv;\r\n}\r\n\r\nvoid main() {\r\n  vec3 nn = normalize(worldNormal);\r\n  vec3 ndir = normalize(worldViewDir);\r\n  vec3 sampleDir = (warpAlpha)*ndir + (1.0 - warpAlpha)*nn;\r\n\r\n  float distToPixel = length(worldViewDir);\r\n  float dd = (distToPixel - warpParams.z) / (warpParams.w - warpParams.z);\r\n  //float dd = 1.0 - distToPixel;\r\n  float dthresh = clamp(dd, 0.0, 1.0);\r\n  vec2 uv = panoMap(sampleDir);\r\n  if(rand(gl_FragCoord.xy) < dthresh) {\r\n    discard;\r\n  }\r\n\r\n  gl_FragColor.rgb = texture2D(src, uv).rgb;\r\n  gl_FragColor.a = 1.0;\r\n}\r\n"
+	module.exports = "#define M_PI 3.1415926535897932384626433832795\r\nuniform sampler2D src;\r\nuniform vec4 warpParams;\r\nvarying vec3 worldNormal;\r\nvarying vec3 worldViewDir;\r\nvarying float warpAlpha;\r\n\r\nfloat rand(vec2 co){\r\n  return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);\r\n}\r\n\r\nvec2 panoMap(vec3 vdir) {\r\n  float r = length(vdir.xz);\r\n  float theta = atan(vdir.z, vdir.x) / M_PI;\r\n  float phi = atan(vdir.y, r) / M_PI;\r\n  vec2 uv = vec2(theta*0.5 + 0.5, phi + 0.5);\r\n  return uv;\r\n}\r\n\r\nvoid main() {\r\n  vec3 nn = normalize(worldNormal);\r\n  vec3 ndir = normalize(worldViewDir);\r\n  vec3 sampleDir = (warpAlpha)*ndir + (1.0 - warpAlpha)*nn;\r\n\r\n  float distToPixel = length(worldViewDir);\r\n  float dd = (distToPixel - warpParams.z) / (warpParams.w - warpParams.z);\r\n  float dthresh = clamp(dd, 0.0, 1.0);\r\n  vec2 uv = panoMap(sampleDir);\r\n  if(rand(gl_FragCoord.xy) < dthresh) {\r\n    discard;\r\n  }\r\n\r\n  gl_FragColor.rgb = texture2D(src, uv).rgb;\r\n  gl_FragColor.a = 1.0;\r\n}\r\n"
 
 /***/ })
 /******/ ]);
--- shaders/vs_pano_warp.glsl
+++ shaders/vs_pano_warp.glsl
@@ -6,7 +6,8 @@ varying float warpAlpha;
 void main(void) {
   vec4 zeroPos = modelMatrix * vec4(0.0, 0.0, 0.0, 1.0);
   float distToZero = length(cameraPosition - zeroPos.xyz);
-  warpAlpha = clamp(warpParams.x * distToZero + warpParams.y, 0.0, 1.0);
+  float dd = (distToZero - warpParams.x) / (warpParams.y - warpParams.x);
+  warpAlpha = clamp(dd, 0.0, 1.0);
   vec4 worldPos = modelMatrix * vec4(position, 1.0);
   worldViewDir = worldPos.xyz - cameraPosition;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
